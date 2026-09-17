97cf65b91480c762e56f1dfaa614e783131bb407
PANKAJ BHATT <109691325+Pnkajbhatt@users.noreply.github.com>
2026-09-17 22:43:36 +0530

    feat: Initialize music-service and user-service with Spring Boot setup
    - Added pom.xml for both services with necessary dependencies including Spring Boot, Spring Cloud, PostgreSQL, and Lombok.
    - Created main application classes (MusicServiceApplication and UserServiceApplication) to bootstrap the Spring Boot applications.
    - Configured application properties in application.yaml for both services.
    - Added test classes (MusicServiceApplicationTests and UserServiceApplicationTests) to ensure context loading.
    - Included Maven Wrapper files for both services to facilitate consistent builds across environments.
    - Set up .gitignore and .gitattributes for proper version control management.

---
+++ history-service/src/test/java/in/pnkj/history_service/HistoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.pnkj.history_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
